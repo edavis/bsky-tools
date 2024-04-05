08eeeed6b2099cd8720935c75fde0f2f7a90b966
Eric Davis <eric@davislv.com>
2024-04-05 14:15:39 -0700

    feeds/battle.py: add functionality

--- feeds/battle.py
+++ feeds/battle.py
@@ -6,23 +6,25 @@ import grapheme
 
 from . import BaseFeed
 
-MAX_TEXT_LENGTH = 140
-
-class RapidFireFeed(BaseFeed):
-    FEED_URI = 'at://did:plc:4nsduwlpivpuur4mqkbfvm6a/app.bsky.feed.generator/rapidfire'
+class BattleFeed(BaseFeed):
+    FEED_URI = 'at://did:plc:4nsduwlpivpuur4mqkbfvm6a/app.bsky.feed.generator/battle'
 
     def __init__(self):
-        self.db_cnx = apsw.Connection('db/rapidfire.db')
+        self.db_cnx = apsw.Connection('db/battle.db')
         self.db_cnx.pragma('journal_mode', 'WAL')
         self.db_cnx.pragma('wal_autocheckpoint', '0')
 
         with self.db_cnx:
             self.db_cnx.execute("""
-            create table if not exists posts (uri text, create_ts timestamp, lang text);
-            create index if not exists create_ts_idx on posts(create_ts);
+            create table if not exists posts (
+                uri text,
+                grapheme_length integer unique,
+                create_ts timestamp,
+                lang text
+            );
             """)
 
-        self.logger = logging.getLogger('feeds.rapidfire')
+        self.logger = logging.getLogger('feeds.battle')
 
     def process_commit(self, commit):
         op = commit['op']
@@ -37,59 +39,57 @@ class RapidFireFeed(BaseFeed):
         if record is None:
             return
 
-        if all([
-            grapheme.length(record['text']) <= MAX_TEXT_LENGTH,
-            record.get('reply') is None,
-            record.get('embed') is None,
-            record.get('facets') is None
-        ]):
-            repo = commit['repo']
-            path = op['path']
-            post_uri = f'at://{repo}/{path}'
-            ts = self.safe_timestamp(record['createdAt']).timestamp()
+        repo = commit['repo']
+        path = op['path']
+        post_uri = f'at://{repo}/{path}'
+        l = grapheme.length(record['text'])
+        ts = self.safe_timestamp(record['createdAt']).timestamp()
 
-            self.transaction_begin(self.db_cnx)
+        self.transaction_begin(self.db_cnx)
 
-            langs = record.get('langs') or ['']
-            for lang in langs:
-                self.db_cnx.execute(
-                    'insert into posts (uri, create_ts, lang) values (:uri, :ts, :lang)',
-                    dict(uri=post_uri, ts=ts, lang=lang)
-                )
-
-    def delete_old_posts(self):
-        self.db_cnx.execute(
-            "delete from posts where create_ts < unixepoch('now', '-15 minutes')"
-        )
+        langs = record.get('langs') or ['']
+        for lang in langs:
+            self.db_cnx.execute("""
+            insert into posts(uri, grapheme_length, create_ts, lang)
+            values(:uri, :length, :ts, :lang)
+            on conflict(grapheme_length) do update set uri = :uri
+            """, dict(uri=post_uri, length=l, ts=ts, lang=lang))
 
     def commit_changes(self):
         self.logger.debug('committing changes')
-        self.delete_old_posts()
         self.transaction_commit(self.db_cnx)
         self.wal_checkpoint(self.db_cnx, 'RESTART')
 
     def serve_feed(self, limit, offset, langs):
-        if '*' in langs:
-            cur = self.db_cnx.execute(
-                "select uri from posts order by create_ts desc limit :limit offset :offset",
-                dict(limit=limit, offset=offset)
-            )
-            return [uri for (uri,) in cur]
-        else:
-            lang_values = list(langs.values())
-            lang_selects = ['select uri, create_ts from posts where lang = ?'] * len(lang_values)
-            lang_clause = ' union '.join(lang_selects)
-            cur = self.db_cnx.execute(
-                lang_clause + ' order by create_ts desc limit ? offset ?',
-                [*lang_values, limit, offset]
-            )
-            return [uri for (uri, create_ts) in cur]
+        # if '*' in langs:
+        #     cur = self.db_cnx.execute(
+        #         "select uri from posts order by grapheme_length asc limit :limit offset :offset",
+        #         dict(limit=limit, offset=offset)
+        #     )
+        #     return [uri for (uri,) in cur]
+        # else:
+        #     lang_values = list(langs.values())
+        #     lang_selects = ['select uri, grapheme_length from posts where lang = ?'] * len(lang_values)
+        #     lang_clause = ' union '.join(lang_selects)
+        #     cur = self.db_cnx.execute(
+        #         lang_clause + ' order by grapheme_length asc limit ? offset ?',
+        #         [*lang_values, limit, offset]
+        #     )
+        #     return [uri for (uri, create_ts) in cur]
+
+        cur = self.db_cnx.execute("""
+        select uri
+        from posts
+        order by grapheme_length asc
+        limit :limit offset :offset
+        """, dict(limit=limit, offset=offset))
+        return [uri for (uri,) in cur]
 
     def serve_feed_debug(self, limit, offset, langs):
         query = """
-        select *, unixepoch('now') - create_ts as age_seconds
+        select *
         from posts
-        order by create_ts desc
+        order by grapheme_length asc
         limit :limit offset :offset
         """
         bindings = dict(limit=limit, offset=offset)
