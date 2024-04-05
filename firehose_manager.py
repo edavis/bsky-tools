70f957154ad468388247b3b88565cc8d71ae36e9
Eric Davis <eric@davislv.com>
2024-04-04 21:12:04 -0700

    Add logging to FirehoseManager

--- firehose_manager.py
+++ firehose_manager.py
@@ -1,4 +1,5 @@
 import apsw
+import logging
 
 class FirehoseManager:
     def __init__(self, fname='firehose.db'):
@@ -6,6 +7,8 @@ class FirehoseManager:
         self.db_cnx.pragma('journal_mode', 'WAL')
         with self.db_cnx:
             self.db_cnx.execute("create table if not exists firehose(key text unique, value text)")
+
+        self.logger = logging.getLogger('feeds.firehose')
 
     def get_sequence_number(self):
         cur = self.db_cnx.execute("select * from firehose where key = 'seq'")
@@ -16,8 +19,12 @@ class FirehoseManager:
         return int(value)
 
     def set_sequence_number(self, value):
+        self.logger.debug(f'setting sequence number = {value}')
+
         with self.db_cnx:
             self.db_cnx.execute(
                 "insert into firehose (key, value) values ('seq', :value) on conflict(key) do update set value = :value",
                 dict(value=value)
             )
+
+        self.db_cnx.pragma('wal_checkpoint(TRUNCATE)')
