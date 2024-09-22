a45e68db8d26e2d611c2061938566bf83b246a6c
Eric Davis <eric@davislv.com>
2024-09-21 22:43:48 -0700

    feat(most-liked): to trim long tail, only move posts to DB once they have some likes
    Also, move back to db/mostliked.db.

--- feeds/mostliked.py
+++ feeds/mostliked.py
@@ -2,10 +2,14 @@ import logging
 
 import apsw
 import apsw.ext
+from expiringdict import ExpiringDict
 import threading
 import queue
 
 from . import BaseFeed
+
+# store post in database once it has this many likes
+MIN_LIKES = 5
 
 class DatabaseWorker(threading.Thread):
     def __init__(self, name, db_path, task_queue):
@@ -47,13 +51,11 @@ class DatabaseWorker(threading.Thread):
 class MostLikedFeed(BaseFeed):
     FEED_URI = 'at://did:plc:4nsduwlpivpuur4mqkbfvm6a/app.bsky.feed.generator/most-liked'
     DELETE_OLD_POSTS_QUERY = """
-    delete from posts where (
-      create_ts < unixepoch('now', '-15 minutes') and likes < 2
-    ) or create_ts < unixepoch('now', '-24 hours');
+    delete from posts where create_ts < unixepoch('now', '-24 hours');
     """
 
     def __init__(self):
-        self.db_cnx = apsw.Connection('/dev/shm/mostliked.db')
+        self.db_cnx = apsw.Connection('db/mostliked.db')
         self.db_cnx.pragma('foreign_keys', True)
         self.db_cnx.pragma('journal_mode', 'WAL')
         self.db_cnx.pragma('wal_autocheckpoint', '0')
@@ -74,9 +76,10 @@ class MostLikedFeed(BaseFeed):
             """)
 
         self.logger = logging.getLogger('feeds.mostliked')
+        self.drafts = ExpiringDict(max_len=50_000, max_age_seconds=5*60)
 
         self.db_writes = queue.Queue()
-        db_worker = DatabaseWorker('mostliked', '/dev/shm/mostliked.db', self.db_writes)
+        db_worker = DatabaseWorker('mostliked', 'db/mostliked.db', self.db_writes)
         db_worker.start()
 
     def process_commit(self, commit):
@@ -86,19 +89,14 @@ class MostLikedFeed(BaseFeed):
         if commit['collection'] == 'app.bsky.feed.post':
             record = commit.get('record')
             post_uri = f"at://{commit['did']}/app.bsky.feed.post/{commit['rkey']}"
-            task = (
-                'insert or ignore into posts (uri, create_ts, likes) values (:uri, :ts, 0)',
-                {'uri': post_uri, 'ts': self.safe_timestamp(record.get('createdAt')).timestamp()}
-            )
-            self.db_writes.put(task)
 
-            langs = record.get('langs', [])
-            for lang in langs:
-                task = (
-                    'insert or ignore into langs (uri, lang) values (:uri, :lang)',
-                    {'uri': post_uri, 'lang': lang}
-                )
-                self.db_writes.put(task)
+            # to keep the DB in check, instead of adding every post right away
+            # we make note of it but only add to DB once it gets some likes
+            self.drafts[post_uri] = {
+                'ts': self.safe_timestamp(record.get('createdAt')).timestamp(),
+                'langs': record.get('langs', []),
+                'likes': 0,
+            }
 
         elif commit['collection'] == 'app.bsky.feed.like':
             record = commit.get('record')
@@ -106,6 +104,28 @@ class MostLikedFeed(BaseFeed):
                 subject_uri = record['subject']['uri']
             except KeyError:
                 return
+
+            if subject_uri in self.drafts:
+                record_info = self.drafts.pop(subject_uri).copy()
+                record_info['likes'] += 1
+                if record_info['likes'] < MIN_LIKES:
+                    self.drafts[subject_uri] = record_info
+                    return
+
+                self.logger.debug(f'graduating {subject_uri}')
+
+                task = (
+                    'insert or ignore into posts (uri, create_ts, likes) values (:uri, :ts, :likes)',
+                    {'uri': subject_uri, 'ts': record_info['ts'], 'likes': record_info['likes']}
+                )
+                self.db_writes.put(task)
+
+                for lang in record_info['langs']:
+                    task = (
+                        'insert or ignore into langs (uri, lang) values (:uri, :lang)',
+                        {'uri': subject_uri, 'lang': lang}
+                    )
+                    self.db_writes.put(task)
 
             subject_exists = self.db_cnx.execute('select 1 from posts where uri = ?', [subject_uri])
             if subject_exists.fetchone() is None:
@@ -120,6 +140,7 @@ class MostLikedFeed(BaseFeed):
     def commit_changes(self):
         self.db_writes.put((self.DELETE_OLD_POSTS_QUERY, {}))
         self.db_writes.put('COMMIT')
+        self.logger.debug(f'there are {len(self.drafts)} drafts')
 
     def generate_sql(self, limit, offset, langs):
         bindings = []
